dcbdfa750f314a74013e9ed3a376209d1af098a4
root <aprajita.kumari@tarento.com>
2018-09-21 16:45:10 +0530

    hide explore sunbird button

--- src/app/client/src/app/modules/core/components/main-header/main-header.component.ts
+++ src/app/client/src/app/modules/core/components/main-header/main-header.component.ts
@@ -102,7 +102,7 @@ export class MainHeaderComponent implements OnInit, OnDestroy {
 
   ngOnInit() {
     try {
-      this.exploreButtonVisibility = (<HTMLInputElement>document.getElementById('exploreButtonVisibility')).value;
+      this.exploreButtonVisibility = 'false'; // (<HTMLInputElement>document.getElementById('exploreButtonVisibility')).value;
     } catch (error) {
       this.exploreButtonVisibility = 'false';
     }
